c2c67f9f140c625a1b97b194f4a7152240246c6b
carolyn_m <carolynlmadden@gmail.com>
2025-03-07 11:02:56 -0500

    Picking favorite quotes part 1

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -2,15 +2,16 @@ import React from "react";
 import QuoteCard from "./QuoteCard";
 import CategoryForm from "./CategoryForm";
 
-function Quotes({ quotes, category, categories, handleCategoryChange }) {
+function Quotes({ quotes, category, categories, handleCategoryChange, addToFavorites }) {
     return (
         <section className="all-quotes">
             <div className="quotes wrapper">
                 <div className="category-header">
+                    <h2>Pick your Favorite Quotes Below</h2>
                     <p>Browse through your collection of quotes</p>
                     <CategoryForm categories={categories} category={category} handleCategoryChange={handleCategoryChange} />
                 </div>
-                {quotes.map((quote) => <QuoteCard key={quote.id} quote={quote} />)}
+                {quotes.map((quote) => <QuoteCard key={quote.id} quote={quote} addToFavorites={addToFavorites} />)}
             </div>
         </section>
     )
